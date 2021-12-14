4895c3c9702545da1a8256295d12c7a50ec5b457
idkxd1234 <96030560+idkxd1234@users.noreply.github.com>
2021-12-14 12:58:22 -0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -900,15 +900,15 @@ class PlayState extends MusicBeatState
 		iconP2.visible = !ClientPrefs.hideHud;
 		add(iconP2);
 		reloadHealthBarColors();
-        
-        var creditTxt:FlxText = new FlxText(4,healthBarBG.y + 20,0,("Port by Doggy"), 24);
+		
+		var creditTxt:FlxText = new FlxText(4,healthBarBG.y + 20,0,("Port by\nDOGGY"), 24);
         creditTxt.scrollFactor.set();
-        creditTxt.setFormat("VCR OSD Mono", 24, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        creditTxt.setFormat("VCR OSD Mono", 24, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
         creditTxt.borderColor = FlxColor.BLACK;
         creditTxt.borderSize = 3;
         creditTxt.borderStyle = FlxTextBorderStyle.OUTLINE;
         add(creditTxt);
-        
+		
 		scoreTxt = new FlxText(0, healthBarBG.y + 36, FlxG.width, "", 20);
 		scoreTxt.setFormat(Paths.font("vcr.ttf"), 20, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		scoreTxt.scrollFactor.set();
@@ -1045,12 +1045,7 @@ class PlayState extends MusicBeatState
 					if(daSong == 'roses') FlxG.sound.play(Paths.sound('ANGRY'));
 					schoolIntro(doof);
 				default:
-					if (dialogueJson == null)
-						startCountdown();
-					else
-					{
-                        startDialogue(dialogueJson);
-					}
+					startCountdown();
 			}
 			seenCutscene = true;
 		} else {
@@ -1143,7 +1138,7 @@ class PlayState extends MusicBeatState
 		{
 			if (!runCutscene)
 		    {
-	            FlxG.switchState(new VideoState2('assets/videos/' + fileName + '.webm', function()
+	            FlxG.switchState(new VideoState('assets/videos/' + fileName + '.webm', function()
 	            {
 	                FlxG.switchState(new PlayState());  
 	                runCutscene = true;                          
@@ -1177,10 +1172,7 @@ class PlayState extends MusicBeatState
 	var dialogueCount:Int = 0;
 	//You don't have to add a song, just saying. You can just do "startDialogue(dialogueJson);" and it should work
 	public function startDialogue(dialogueFile:DialogueFile, ?song:String = null):Void
-	{
-		#if mobileC
-		mcontrols.visible = false;
-		#end	
+	{	
 		// TO DO: Make this more flexible, maybe?
 		if(dialogueFile.dialogue.length > 0) {
 			inCutscene = true;
@@ -1302,9 +1294,6 @@ class PlayState extends MusicBeatState
 
 	public function startCountdown():Void
 	{
-		#if mobileC
-		mcontrols.visible = true;
-		#end
 		if(startedCountdown) {
 			callOnLuas('onStartCountdown', []);
 			return;
@@ -1313,6 +1302,9 @@ class PlayState extends MusicBeatState
 		inCutscene = false;
 		var ret:Dynamic = callOnLuas('onStartCountdown', []);
 		if(ret != FunkinLua.Function_Stop) {
+                        #if mobileC
+		        mcontrols.visible = true;
+		        #end
 			generateStaticArrows(0);
 			generateStaticArrows(1);
 			for (i in 0...playerStrums.length) {
